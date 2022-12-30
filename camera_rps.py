e012c44407bf0855c03dcb701aa66667806f841a
luketl98 <luketl98@hotmail.com>
2022-12-30 18:15:13 +0000

    update

--- camera_rps.py
+++ camera_rps.py
@@ -5,37 +5,48 @@ model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-while True: 
-    ret, frame = cap.read()
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-    image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-    data[0] = normalized_image
-    prediction = model.predict(data)
-    cv2.imshow('frame', frame)
-    print(prediction)
+def get_prediction():
+    while True: 
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        data[0] = normalized_image
+        prediction = model.predict(data)
+        cv2.imshow('frame', frame)
+        print(prediction)
 
-    result = np.argmax(prediction)
-    print(result)
-
-    if result == 0:
-        result = 'Rock'
-        print(result)
-    elif result == 1:
-        result = 'Paper'
-        print(result)
-    elif result == 2:
-        result = 'Scissors'
-        print(result)
-    elif result == 3:
-        result = 'Nothing'
+    # Find the result using numpy and print
+        result = np.argmax(prediction)
         print(result)
 
-    # Press q to close the window
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Assign the result
+        if result == 0:
+            result = 'Rock'
+            print(result)
+        elif result == 1:
+            result = 'Paper'
+            print(result)
+        elif result == 2:
+            result = 'Scissors'
+            print(result)
+        elif result == 3:
+            result = 'Nothing'
+            print(result)
             
-# After the loop release the cap object
-cap.release()
-# Destroy all the windows
-cv2.destroyAllWindows()
+        
+        # Press q to close the window
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            
+                
+    # After the loop release the cap object
+    cap.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
+    # Reprint the result
+    print(result)
+    # Return the result 
+    return result
+    
